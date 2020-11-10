b767ff9bd02db9544def8590d5e2785fdd7f5dfe
jherna76 <jherna76@elpuig.xeill.net>
2020-11-10 16:04:14 +0100

    static members

--- src/StaticExample/BankCommons.java
+++ src/StaticExample/BankCommons.java
@@ -3,7 +3,7 @@ package StaticExample;
 public final class BankCommons {
     public static final float TAE = 4.5f;
     public static final String PREFIX_NAME = "BPC"; //Banc Puig Castellar
-    public static enum WorkDay {
+    public enum WorkDay {
         MONDAY, TUESDAY, WEDNESDAY,
         THURSDAY, FRIDAY
     }
--- src/StaticExample/MainStatics.java
+++ src/StaticExample/MainStatics.java
@@ -6,6 +6,8 @@ public class MainStatics {
         Draws.title("exemple membres statics");
         Draws.title(BankCommons.PREFIX_NAME);
 
+        Draws.title(BankCommons.WorkDay.values()[0].toString());
+
 
     }
 }
